a8f1e71c5622bcfe0dddecef7fbef98366680297
Manuel Ulate <fr3zs09@gmail.com>
2023-02-18 15:20:10 -0600

    3d fixed position

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,11 +5,14 @@ export const AboutSection = () => {
   return (
     <SectionFull>
         <div className="grid grid-cols-4 h-full p-5">
-            <div className='flex justify-center col-span-4'>
-                <h2 className="text-4xl">Acerca de mí</h2>
-            </div>
-            <div className='col-span-2 bg-blue'>
-                <Scene />
+            <div className='flex justify-between items-center col-span-4'>
+                <div>
+                  <h2 className="text-4xl">Acerca de mí</h2>
+                  <p className='text-sm'>
+                    Mi nombre es Manuel Ulate Sancho, vivo en San Ramón de Alajuela, Costa Rica
+                  </p>
+                </div>
+                <Scene/>
             </div>
         </div>
     </SectionFull>
