e7cd9f4c2f18904bb69516fb59f38b000ed88b66
Manuel Ulate <fr3zs09@gmail.com>
2023-02-19 16:03:29 -0600

    changes again on main sec

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,19 +3,8 @@ import { ThreeDCircle, ThreeDWrapper, SectionFull } from "./"
 export const MainSection = () => {
   return (
     <SectionFull>
-        <div className="flex flex-row items-center relative h-full w-full">
-
-          <ThreeDWrapper >
-            <ThreeDCircle position={[ 2, 0, 0 ]}  scale={ 0.5 } />
-            <ThreeDCircle position={[ 8, 0, 0 ]}  scale={ 1.2 } />
-            <ThreeDCircle position={[ -3, 3, 0 ]} scale={ 0.3 } />
-            <ThreeDCircle position={[ 4, -3, 0 ]}  scale={ 0.5 } />
-            <ThreeDCircle position={[ 5, 4, 0 ]} scale={ 1.2 } />
-            <ThreeDCircle position={[ 2, 0, 0 ]}  scale={ 0.5 } />
-            <ThreeDCircle position={[ -4, -4, 0 ]} scale={ 1.7 } />
-            <ThreeDCircle position={[ -6, 4, 0 ]} scale={ 1.2 } />
-          </ThreeDWrapper>
-          <div className="fadeIn absolute z-10">
+        <div className="flex flex-row items-center relative p-5 h-full w-full">
+          <div className="fadeIn absolute z-10 ">
             <h1 className="text-6xl font-bold">Manuel Ulate Sancho</h1>
             <h2 className="text-2xl">Front End Developer & UX/UI Designer</h2>
           </div>
