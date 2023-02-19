69e9885b41348dbd65637d76d3eafba50f84dbd3
Manuel Ulate <fr3zs09@gmail.com>
2023-02-19 15:26:47 -0600

    new main section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,13 +3,13 @@ import { ThreeDCircle, ThreeDWrapper, SectionFull } from "./"
 export const MainSection = () => {
   return (
     <SectionFull>
-        <div className="flex flex-row items-center h-full">
+        <div className="flex flex-row items-center h-full p-5">
 
-          <div className="flex-grow-1 bg-transparent ml-5 absolute z-10">
+          <div className="flex-grow-1">
             <h1 className="text-6xl font-bold">Manuel Ulate Sancho</h1>
             <h2 className="text-2xl">Front End Developer & UX/UI Designer</h2>
           </div>
-          <ThreeDWrapper
+          {/* <ThreeDWrapper
             className='three-main-section'
           >
             <ThreeDCircle position={[ 2, 0, 0 ]}  scale={ 0.5 } />
@@ -20,9 +20,19 @@ export const MainSection = () => {
             <ThreeDCircle position={[ 2, 0, 0 ]}  scale={ 0.5 } />
             <ThreeDCircle position={[ -4, -4, 0 ]} scale={ 1.7 } />
             <ThreeDCircle position={[ -6, 4, 0 ]} scale={ 1.2 } />
-          </ThreeDWrapper>
-
+          </ThreeDWrapper> */}
+          <div className="flex h-full items-center w-full">
+            <Character />
+          </div>
         </div>
       </SectionFull>
   )
+}
+
+import Spline from '@splinetool/react-spline';
+
+const Character = () => {
+  return (
+    <Spline scene="https://prod.spline.design/gaJM8-5es03RO3v9/scene.splinecode" />
+  );
 }
---
+++ src/components/TechSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionFull } from './Section'
+
+export const TechSection = () => {
+  return (
+    <SectionFull>
+        <h1>Hola Mundo</h1>
+    </SectionFull>
+  )
+}
